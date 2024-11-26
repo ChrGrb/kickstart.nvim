ee033390a1fdc9a733fe90da7b2849ead940848d
Christoph Gruber <christoph@mvst.co>
2024-11-26 15:45:11 +0100

    Update

---
+++ lua/custom/plugins/autotag.lua
@@ -0,0 +1,9 @@
+return {
+  'https://github.com/windwp/nvim-ts-autotag.git',
+  opts = {
+    -- Defaults
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = false, -- Auto close on trailing </
+  },
+}
